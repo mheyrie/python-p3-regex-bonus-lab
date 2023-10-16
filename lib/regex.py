444bb3f4193c2cd4c166368f89d09aa21b0c2783
Mary <omota4fidelity@gmail.com>
2023-10-16 18:15:14 +0100

    completed

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,14 @@
 import re
 
-my_pattern = r""
+FINDALL_STRING = """
+    It's such a lovely day today.
+    Where'd all the time go?
+    Some weather we're having today, huh?
+    Tomorrow never knows!
+    Maybe today's just not my day.
+    It's clobbering time!
+"""
+
+my_pattern = r"It's such a lovely day today\.|Some weather we're having today, huh\?|Maybe today's just not my day\."
 my_regex = re.compile(my_pattern)
 
